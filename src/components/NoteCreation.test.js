a0b1c0dffe7b0df4a64207168d42c2e6497a3814
Moncef Fadal <genjurosama@gmail.com>
2023-09-20 18:43:27 +0200

    Added more unit tests

--- src/components/NoteCreation.test.js
+++ src/components/NoteCreation.test.js
@@ -1,41 +1,69 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import NoteCreation from './NoteCreation';
-import { ToastContainer, toast } from 'react-toastify';
+import { API, graphqlOperation } from 'aws-amplify';
 
-// Mock the useNavigate function from react-router-dom
+// Mock the AWS Amplify API module
+jest.mock('aws-amplify');
+
+// Mock the useNavigate hook
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: () => jest.fn(),
 }));
 
-// Mock the API.graphql method from aws-amplify
-jest.mock('aws-amplify', () => ({
-  API: {
-    graphql: jest.fn(() => Promise.resolve()),
-  },
-}));
+describe('NoteCreation', () => {
+  it('handles form submission', async () => {
+    // Mock the API.graphql method to resolve with a successful response
+    API.graphql.mockResolvedValueOnce({
+      data: {
+        createPatientNote: {
+          id: '1',
+          patientName: 'John Doe',
+          date: '2023-09-19',
+          medicalObservations: 'Sample note',
+        },
+      },
+    });
 
-// Mock the toast methods from react-toastify
-jest.mock('react-toastify', () => ({
-  ...jest.requireActual('react-toastify'),
-  toast: {
-    success: jest.fn(),
-    error: jest.fn(),
-  },
-}));
+    // Mock the navigate function
+    const navigate = jest.fn();
 
-describe('NoteCreation Component', () => {
-  it('renders without errors', () => {
-    const { getByText, getByPlaceholderText } = render(<NoteCreation />);
+    // Replace the mock for useNavigate
+    jest.spyOn(require('react-router-dom'), 'useNavigate').mockImplementation(() => navigate);
+
+    // Render the component
+    const { getByPlaceholderText, getByText } = render(<NoteCreation />);
+
+    // Fill out the form
+    fireEvent.change(getByPlaceholderText('Patient Full Name'), {
+      target: { value: 'John Doe' },
+    });
+    fireEvent.change(getByPlaceholderText('Date'), {
+      target: { value: '2023-09-19' },
+    });
+    fireEvent.change(getByPlaceholderText('Medical Observations'), {
+      target: { value: 'Sample note' },
+    });
+
+    expect(getByText('Create Note')).toBeInTheDocument()
+    // Click the "Create Note" button
+    fireEvent.click(getByText('Create Note'));
+
+    // Wait for the component to re-fetch and render the data
+    await waitFor(() => {
+      // Assert that the success message is displayed
+      console.log('testing')
+      expect(API.graphql).toHaveBeenCalledWith(
+        graphqlOperation(expect.stringContaining('CreatePatientNote'), {
+          input: {
+            patientName: 'John Doe',
+            date: '2023-09-19',
+            medicalObservations: 'Sample note',
+          },
+        })
+      );
+    });
     
-    // Check if the component renders without errors
-    expect(getByText('Create Patient Note')).toBeInTheDocument();
-    expect(getByPlaceholderText('Patient Full Name')).toBeInTheDocument();
-    expect(getByPlaceholderText('Date')).toBeInTheDocument();
-    expect(getByPlaceholderText('Medical Observations')).toBeInTheDocument();
-    expect(getByText('Create Note')).toBeInTheDocument();
   });
-
-
 });
--- src/components/PatientNotes.test.js
+++ src/components/PatientNotes.test.js
@@ -1,53 +1,99 @@
 import React from 'react';
-import { render, waitFor, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'; // For expect(...).toBeInTheDocument()
-import { API } from 'aws-amplify';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import PatientNotes from './PatientNotes';
+import { API, graphqlOperation } from 'aws-amplify';
 
-// Mocking API calls
+// Mock the AWS Amplify API module
 jest.mock('aws-amplify');
 
-// Mocked data for testing
-const mockNotesData = {
-  data: {
-    listPatientNotes: {
-      items: [
-        {
-          id: '1',
-          patientName: 'John Doe',
-          date: '2023-09-20',
-          medicalObservations: 'Some observations',
-        },
-        // Add more mock notes as needed
-      ],
-    },
+// Mock the react-toastify module to prevent toast notifications
+jest.mock('react-toastify', () => ({
+  toast: {
+    success: jest.fn(),
+    error: jest.fn(),
   },
-};
+  ToastContainer: jest.fn(),
+}));
 
-describe('PatientNotes Component', () => {
-  beforeEach(() => {
-    // Reset the mock implementation for each test
-    API.graphql.mockReset();
-  });
+describe('PatientNotes', () => {
+  it('fetches and displays patient notes', async () => {
+    // Mock the API.graphql method to resolve with a list of patient notes
+    API.graphql.mockResolvedValueOnce({
+      data: {
+        listPatientNotes: {
+          items: [
+            {
+              id: '1',
+              patientName: 'John Doe',
+              date: '2023-09-19',
+              medicalObservations: 'Sample note 1',
+            },
+            {
+              id: '2',
+              patientName: 'Jane Smith',
+              date: '2023-09-20',
+              medicalObservations: 'Sample note 2',
+            },
+          ],
+        },
+      },
+    });
 
-  it('renders the component with data', async () => {
-    // Mocking the API response
-    API.graphql.mockResolvedValueOnce(mockNotesData);
-  
+    // Render the component
     const { getByText } = render(<PatientNotes />);
-  
-    // Wait for the component to fetch and render the data
+
+    // Wait for the component to load
     await waitFor(() => {
       expect(getByText('John Doe')).toBeInTheDocument();
-      expect(getByText('2023-09-20')).toBeInTheDocument();
-      expect(getByText('Some observations')).toBeInTheDocument();
+      expect(getByText('Jane Smith')).toBeInTheDocument();
     });
   });
-  
 
- 
+  it('handles note deletion', async () => {
+    // Mock the API.graphql method for fetching patient notes
+    API.graphql.mockResolvedValueOnce({
+      data: {
+        listPatientNotes: {
+          items: [
+            {
+              id: '1',
+              patientName: 'John Doe',
+              date: '2023-09-19',
+              medicalObservations: 'Sample note 1',
+            },
+          ],
+        },
+      },
+    });
 
-  
+    // Render the component
+    const { getByTestId, queryByText } = render(<PatientNotes />);
 
-  // Add more test cases for interactions, such as opening the modal, etc.
+    // Wait for the component to load
+    await waitFor(() => {
+      expect(queryByText('John Doe')).toBeInTheDocument();
+    });
+
+    // Mock the API.graphql method for note deletion
+    API.graphql.mockResolvedValueOnce({});
+
+    // Click the "Delete" button
+    fireEvent.click(getByTestId('delete-button-1'));
+
+    // Wait for the component to re-fetch and render the updated data
+    await waitFor(() => {
+      // Assert that the note is no longer in the DOM
+      expect(queryByText('John Doe')).not.toBeInTheDocument();
+    });
+
+    // Assert that the API.graphql method was called with the correct input
+    expect(API.graphql).toHaveBeenCalledWith(
+      graphqlOperation(expect.stringContaining('DeletePatientNote'), {
+        input: { id: '1' },
+      })
+    );
+  });
+
+
+
 });
